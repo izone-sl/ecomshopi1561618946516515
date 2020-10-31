bb4878786835c8bb22cc43d690a513bf1293fa5c
Mohamed Raza <59387235+Raza9798@users.noreply.github.com>
2020-10-31 21:28:52 +0530

    commit home ui

---
+++ src/components/Footer.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="Footer">
+    <v-row class="m-0">
+      <div style="height:5px; background-color:#004D40; width:100%"></div>
+    </v-row>
+
+    <v-row class="m-0">
+      <div class="col-xl-12  ">
+        <v-row>
+          <div class="col-xl-4  ">
+            <span class="ml-3"> Connect with us</span>
+            <br />
+            <a href="" class="btn d-flex align-center">
+              <v-icon class="text-primary">mdi-facebook </v-icon> Like us on
+              Facebook
+            </a>
+            <a href="" class="btn d-flex align-center">
+              <v-icon class="text-primary">mdi-instagram </v-icon> Follow us on
+              Facebook
+            </a>
+          </div>
+          <div class="col-xl-2  ">
+            <span class="ml-3">Learn More</span>
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">How to sell fast</a>
+            <br />
+            <a href="" class="btn" style="font-size:15px; "
+              >Doorstep Delivery</a
+            >
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">Membership</a>
+            <br />
+            <a href="" class="btn" style="font-size:15px; "
+              >Banner Advertising</a
+            >
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">Promote your ad</a>
+          </div>
+          <div class="col-xl-2  ">
+            <span class="ml-3"> Help & Support</span>
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">FAQ</a>
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">Stay safe withus</a>
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">Contact us</a>
+          </div>
+          <div class="col-xl-2  ">
+            <span class="ml-3"> Social</span>
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">Blog</a><br />
+            <a href="" class="btn" style="font-size:15px; ">Facebook</a><br />
+            <a href="" class="btn" style="font-size:15px; ">Twitter</a><br />
+            <a href="" class="btn" style="font-size:15px; ">YouTube</a>
+          </div>
+          <div class="col-xl-2  ">
+            <span class="ml-3"> About us</span>
+            <br />
+            <a href="" class="btn" style="font-size:15px; ">About us</a><br />
+            <a href="" class="btn" style="font-size:15px; ">Career</a><br />
+            <a href="" class="btn" style="font-size:15px; "
+              >Terms & Conditions</a
+            ><br />
+            <a href="" class="btn" style="font-size:15px; ">Privacy Policy</a
+            ><br />
+            <a href="" class="btn" style="font-size:15px; ">Sitemap</a><br />
+            <br />
+          </div>
+        </v-row>
+        <v-row>
+          <div class="col-xl-12  ">
+            <v-toolbar flat>
+              <span
+                style="font-size:15px; color:#546E7A"
+                class="text-dark ml-10 "
+              >
+                Copyright © Shopikart.lk</span
+              >
+              <v-spacer></v-spacer>
+
+              <v-avatar class="mr-8">
+                <!-- <img src="../assets/exlogo.png" alt="Logo" /> -->
+              </v-avatar>
+            </v-toolbar>
+          </div>
+        </v-row>
+      </div>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  methods: {
+    dashboard_redirect() {
+      this.$router.push({
+        name: "Login",
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div id="Home">
     <!-- TOP NAVIGATION -->
 
-    <v-toolbar dense height="65" color="#0c21e4" dark>
+    <v-toolbar dense height="65" color="#375bd0" dark>
       <v-app-bar-nav-icon class="c_menu"></v-app-bar-nav-icon> menu
       <!-- <v-avatar class="c_avatar">
         <v-img src="../assets/logo.png"></v-img>
@@ -22,14 +22,14 @@
       ></v-text-field>
       <v-spacer></v-spacer>
 
-      <v-btn depressed class="c_login_btn" color="#0c21e4">
+      <v-btn depressed class="c_login_btn" color="#375bd0">
         <v-icon small>mdi-login</v-icon> Login
       </v-btn>
     </v-toolbar>
 
     <!-- MOBILE ONLY SEARCH FUNCTION -->
     <v-card
-      color="#0c21e4"
+      color="#375bd0"
       flat
       class="c_m_search_card rounded-0 border-0"
       height="70"
@@ -93,13 +93,13 @@
       </v-carousel-item>
     </v-carousel>
 
+    <!-- MOBILE ONLY PRODUCT DISPLAY -->
     <!-- LATEST PRODUCTS -->
-
     <v-card class="mt-3" flat tile>
-      <v-toolbar dark color="#0c21e4" dense class="rounded-0 border-0">
+      <v-toolbar dark color="#375bd0" dense class="rounded-0 border-0">
         <v-toolbar-title>LATEST PRODUCTS</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn color="#0c21e4" depressed>
+        <v-btn color="#375bd0" depressed>
           View All
         </v-btn>
       </v-toolbar>
@@ -116,15 +116,13 @@
         </v-tab>
       </v-tabs>
     </v-card>
-
-    <!-- MOBILE ONLY PRODUCT DISPLAY -->
     <v-card class="mobile_product_view" tile flat>
-      <v-row class="m-0 ">
-        <!-- P1 -->
-        <v-col cols="6">
-          <v-card class="border border-primary rounded-0  pa-1">
+      <v-row class="m-0  pa-2" style="background-color:#375bd0;">
+        <!-- p1 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1" width="100%">
             <v-img src="../assets/products/135.png" height="100px"></v-img>
-            <v-card-title style="font-size:18px;">
+            <v-card-title style="font-size:16px;">
               RATTHI MILK
             </v-card-title>
             <v-card-subtitle class="text-success">
@@ -138,12 +136,11 @@
             </v-card-actions>
           </v-card>
         </v-col>
-
         <!-- P2 -->
-        <v-col cols="6">
+        <v-col cols="6" class="bg-light ">
           <v-card class="border border-primary rounded-0  pa-1">
             <v-img src="../assets/products/2.png" height="100px"></v-img>
-            <v-card-title style="font-size:18px;">
+            <v-card-title style="font-size:16px;">
               ANCHOR MILK
             </v-card-title>
             <v-card-subtitle class="text-success">
@@ -159,10 +156,10 @@
         </v-col>
 
         <!-- P3 -->
-        <v-col cols="6">
+        <v-col cols="6" class="bg-light ">
           <v-card class="border border-primary rounded-0  pa-1">
             <v-img src="../assets/products/milo.jpg" height="100px"></v-img>
-            <v-card-title style="font-size:18px;">
+            <v-card-title style="font-size:16px;">
               MILO DRINK
             </v-card-title>
             <v-card-subtitle class="text-success">
@@ -178,11 +175,11 @@
         </v-col>
 
         <!-- P4 -->
-        <v-col cols="6">
+        <v-col cols="6" class="bg-light ">
           <v-card class="border border-primary rounded-0  pa-1">
             <v-img src="../assets/products/72.png" height="100px"></v-img>
-            <v-card-title style="font-size:18px;">
-              SUNLIGHT SOAP
+            <v-card-title style="font-size:16px;">
+              SUNLIGHT
             </v-card-title>
             <v-card-subtitle class="text-success">
               Rs. 60
@@ -197,6 +194,83 @@
         </v-col>
       </v-row>
     </v-card>
+
+    <!-- MOBILE ONLY PRODUCT DISPLAY -->
+    <!-- <v-card class="mobile_product_view" tile flat>
+      <v-row class="m-0 ">
+        <v-col cols="6">
+          <v-card class="border border-primary rounded-0  pa-1" width="100%">
+            <v-img src="../assets/products/135.png" height="100px"></v-img>
+            <v-card-title style="font-size:16px;">
+              RATTHI MILK
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 300
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img src="../assets/products/2.png" height="100px"></v-img>
+            <v-card-title style="font-size:16px;">
+              ANCHOR MILK
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 360
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img src="../assets/products/milo.jpg" height="100px"></v-img>
+            <v-card-title style="font-size:16px;">
+              MILO DRINK
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 75
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img src="../assets/products/72.png" height="100px"></v-img>
+            <v-card-title style="font-size:16px;">
+              SUNLIGHT SOAP
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 60
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card> -->
 
     <!-- advertisement -->
     <v-row class="ma-0">
@@ -213,26 +287,360 @@
       </v-col>
     </v-row>
 
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    <!-- MOBILE ONLY PRODUCT DISPLAY -->
+    <!-- KITCHEN PRODUCTS -->
+    <v-card class="mt-3" flat tile>
+      <v-toolbar dark color="#375bd0" dense class="rounded-0 border-0">
+        <v-toolbar-title>KITCHEN EQUIPMENTS</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="#375bd0" depressed>
+          View All
+        </v-btn>
+      </v-toolbar>
+
+      <!-- DESKTOP ONLY PRODUCT SLIDER -->
+      <v-tabs light height="250" show-arrows class="product_slider">
+        <v-tab v-for="i in 30" :key="i" class="  ma-1">
+          <v-card width="170" class="pa-0" tile flat>
+            <v-img src="../assets/products/135.png" width="100%"></v-img>
+            <br />
+            <p>RATHI MILK POWDER</p>
+            <p>Rs. 600</p>
+          </v-card>
+        </v-tab>
+      </v-tabs>
+    </v-card>
+    <v-card class="mobile_product_view" tile flat>
+      <v-row class="m-0  pa-2" style="background-color:#375bd0;">
+        <!-- p1 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1" width="100%">
+            <v-img
+              src="../assets/products/ATF0036-Sokany-Healthy-Air-Fryer-5-L-Free-Hopper-pan-001.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              Air-Fryer
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 2300
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- P2 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/Bright-rice-cooker-6L-1.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              RICE COOKER
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 3360
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- P3 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/SHX1305-National-Glass-Top-Double-Burner-01.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              BURNER
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 1075
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- P4 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/SHX1307-Rich-Power-Mixer-Grinder-550-wats-01.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              MIXER
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 3460
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+
+    <!-- MOBILE ONLY PRODUCT DISPLAY -->
+    <!-- SPORTS PRODUCTS -->
+    <v-card class="mt-3" flat tile>
+      <v-toolbar dark color="#375bd0" dense class="rounded-0 border-0">
+        <v-toolbar-title>SPORTS EQUIPMENTS</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="#375bd0" depressed>
+          View All
+        </v-btn>
+      </v-toolbar>
+
+      <!-- DESKTOP ONLY PRODUCT SLIDER -->
+      <v-tabs light height="250" show-arrows class="product_slider">
+        <v-tab v-for="i in 30" :key="i" class="  ma-1">
+          <v-card width="170" class="pa-0" tile flat>
+            <v-img src="../assets/products/135.png" width="100%"></v-img>
+            <br />
+            <p>RATHI MILK POWDER</p>
+            <p>Rs. 600</p>
+          </v-card>
+        </v-tab>
+      </v-tabs>
+    </v-card>
+    <v-card class="mobile_product_view" tile flat>
+      <v-row class="m-0  pa-2" style="background-color:#375bd0;">
+        <!-- p1 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1" width="100%">
+            <v-img
+              src="../assets/products/144148919-97535050-1551920578.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              BADMINTON
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 700
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- P2 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/147539711-102363238-1565744707.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              SOFT BALLS
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 320
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- P3 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/150355619-108536971-1595397194.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              CARROM BOARD
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 1075
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- P4 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/BAT1463829644.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              HARDBALL BAT
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 2500
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+
+    <!-- MOBILE ONLY PRODUCT DISPLAY -->
+    <!-- SPORTS PRODUCTS -->
+    <v-card class="mt-3" flat tile>
+      <v-toolbar dark color="#375bd0" dense class="rounded-0 border-0">
+        <v-toolbar-title>COSMATICS ITEMS</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="#375bd0" depressed>
+          View All
+        </v-btn>
+      </v-toolbar>
+
+      <!-- DESKTOP ONLY PRODUCT SLIDER -->
+      <v-tabs light height="250" show-arrows class="product_slider">
+        <v-tab v-for="i in 30" :key="i" class="  ma-1">
+          <v-card width="170" class="pa-0" tile flat>
+            <v-img src="../assets/products/135.png" width="100%"></v-img>
+            <br />
+            <p>RATHI MILK POWDER</p>
+            <p>Rs. 600</p>
+          </v-card>
+        </v-tab>
+      </v-tabs>
+    </v-card>
+    <v-card class="mobile_product_view" tile flat>
+      <v-row class="m-0  pa-2" style="background-color:#375bd0;">
+        <!-- p1 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1" width="100%">
+            <v-img
+              src="../assets/products/2449771395_770677398-560x636.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              EARRING MODEL
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 700
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- P2 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/12291861545_1595973670-560x636.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              NECK CHAIN
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 320
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- P3 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/4317724898_727322799-560x636.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              LEG CHAIN
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 1075
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- P4 -->
+        <v-col cols="6" class="bg-light ">
+          <v-card class="border border-primary rounded-0  pa-1">
+            <v-img
+              src="../assets/products/12328795990_1595973670-560x636.jpg"
+              height="100px"
+            ></v-img>
+            <v-card-title style="font-size:16px;">
+              MODEL CHAIN
+            </v-card-title>
+            <v-card-subtitle class="text-success">
+              Rs. 2500
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn icon> <v-icon>mdi-cart</v-icon></v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon> <v-icon>mdi-whatsapp</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+
+    <Footer />
+    
   </div>
 </template>
 
 <script>
+import Footer from "../components/Footer";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Footer,
+  },
   data: () => ({
     items: [
       "https://shoppingo.lk/admin_area/slides_images/4s.jpg",
@@ -244,7 +652,7 @@ export default {
 </script>
 <style scoped>
 .product_section_bar {
-  background-color: #0c21e4;
+  background-color: #375bd0;
 }
 
 /* small mobile size */
